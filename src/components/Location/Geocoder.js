8156b60d91f6b3a7ee671e9ec8dc238ebed408c8
Wil Wardrop <wwardrop@gmail.com>
2022-03-09 22:25:13 -0600

    mapping start

--- src/components/Location/Geocoder.js
+++ src/components/Location/Geocoder.js
@@ -7,7 +7,7 @@ import { useState } from "react"
 export const Geocoder = () => {
     const [geolocation, setGeolocation] = useState({})
 
-        return fetch("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAOOul3NMA8auyFgtWcWUCuDsSthTlqLKM")
+        return fetch("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=")
         .then (res => res.json())
         .then((response) => {
             setGeolocation(response)
